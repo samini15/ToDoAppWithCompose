a9c12e66f7a43eba4d5acb2a821afe189595bd2b
Shayan <shayan.amini76@gmail.com>
2023-07-20 15:45:17 +0200

    Dao + Room database

---
+++ app/src/main/java/com/example/todoappwithcompose/data/ToDoDatabase.kt
@@ -0,0 +1,12 @@
+package com.example.todoappwithcompose.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.todoappwithcompose.data.model.ToDoTask
+
+@Database(entities = [ToDoTask::class],
+    version = 1,
+    exportSchema = false)
+abstract class ToDoDatabase : RoomDatabase() {
+    abstract fun toDoDao(): ToDoDao
+}
