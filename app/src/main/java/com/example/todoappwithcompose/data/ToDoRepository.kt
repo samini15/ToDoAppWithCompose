040c8420eaba2e4944a0ce90a8a61c6dc3f5c460
Shayan <shayan.amini76@gmail.com>
2023-07-20 17:08:07 +0200

    Repository + ViewModel

---
+++ app/src/main/java/com/example/todoappwithcompose/data/ToDoRepository.kt
@@ -0,0 +1,28 @@
+package com.example.todoappwithcompose.data
+
+import com.example.todoappwithcompose.data.model.ToDoTask
+import dagger.hilt.android.scopes.ViewModelScoped
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+@ViewModelScoped
+class ToDoRepository @Inject constructor(private val toDoDao: ToDoDao) {
+
+    fun fetchAllTasks() : Flow<List<ToDoTask>> = toDoDao.getAllTasks()
+
+    fun sortByLowPriority() : Flow<List<ToDoTask>> = toDoDao.sortByLowPriority()
+
+    fun sortByLHighPriority() : Flow<List<ToDoTask>> = toDoDao.sortByHighPriority()
+
+    fun findSelectedTask(taskId: Int) : Flow<ToDoTask> = toDoDao.findSelectedTask(taskId)
+
+    suspend fun addTask(toDoTask: ToDoTask) = toDoDao.insertTask(toDoTask)
+
+    suspend fun updateTask(toDoTask: ToDoTask) = toDoDao.updateTask(toDoTask)
+
+    suspend fun deleteTask(toDoTask: ToDoTask) = toDoDao.deleteTask(toDoTask)
+
+    suspend fun deleteAllTasks() = toDoDao.deleteAllTasks()
+
+    fun searchDatabase(searchQuery: String) : Flow<List<ToDoTask>> = toDoDao.searchDatabase(searchQuery)
+}
---
+++ app/src/main/java/com/example/todoappwithcompose/viewModel/ToDoSharedViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.todoappwithcompose.viewModel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.todoappwithcompose.data.ToDoRepository
+import com.example.todoappwithcompose.data.model.ToDoTask
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ToDoSharedViewModel @Inject constructor(
+    private val toDoRepository: ToDoRepository
+): ViewModel() {
+
+    private val _currentTasks = MutableStateFlow<List<ToDoTask>>(emptyList())
+    val currentTask: StateFlow<List<ToDoTask>> = _currentTasks
+
+
+    fun fetchAllTasks() = viewModelScope.launch {
+        toDoRepository.fetchAllTasks().collect() {
+            _currentTasks.value = it
+        }
+    }
+}
